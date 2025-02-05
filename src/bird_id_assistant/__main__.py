3dd53b8bc518489941559161532188ec90c534bf
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-02-05 19:19:24 +0200

    Add CLI entry point

---
+++ src/bird_id_assistant/__main__.py
@@ -0,0 +1,4 @@
+from bird_id_assistant.cli import main
+
+if __name__ == "__main__":
+    main()
---
+++ src/bird_id_assistant/cli.py
@@ -0,0 +1,6 @@
+def main():
+    print("Nothing here yet :)")
+
+
+if __name__ == "__main__":
+    main()
