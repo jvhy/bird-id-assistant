0e46c53a09c6e7115e14f75393830d6015aadb87
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-07-04 13:06:01 +0300

    Move db command line tool to the CLI module

--- src/bird_id_assistant/cli.py
+++ src/bird_id_assistant/cli.py
@@ -2,6 +2,7 @@ import argparse
 import asyncio
 
 from bird_id_assistant.assistant import chat
+from bird_id_assistant.db import create_vector_db, query_vector_db
 from bird_id_assistant.data_collection import collect_data
 from bird_id_assistant.util import dir_path
 
@@ -38,18 +39,66 @@ def parse_args(argv=None):
         help="Number of threads used for making requests (default: %(default)s)"
     )
 
+    # Commands for creating/querying chroma db
+    parser_db = subparsers.add_parser(
+        name="db",
+        help="Commands for interacting with the chroma vector database"
+    )
+    parser_db.add_argument(
+        "--host",
+        type=str,
+        default="localhost",
+        help="ChromaDB client host URL (default: %(default)s)"
+    )
+    parser_db.add_argument("--port", type=int, default=8000, help="ChromaDB client port (default: %(default)s)")
+
+    subparsers_db = parser_db.add_subparsers(dest="db_cmd")
+    parser_db_create = subparsers_db.add_parser("create", help="Create vector database from text files in input dir")
+    parser_db_create.add_argument(
+        "input_doc_dir",
+        type=dir_path,
+        help="Directory containing text documents to store in vector database (default: %(default)s)"
+    )
+    parser_db_create.add_argument(
+        "--collection-name",
+        type=str,
+        default="bia_data",
+        help="Name of the collection where documents are inserted"
+    )
+
+    parser_db_query = subparsers_db.add_parser("query", help="Query documents from vector database")
+    parser_db_query.add_argument("query", type=str, help="Query string")
+    parser_db_query.add_argument(
+        "-n", "--n_results",
+        type=int,
+        default=1,
+        help="Number of closest matching documents to retrieve"
+    )
+    parser_db_query.add_argument(
+        "--collection-name",
+        type=str,
+        default="bia_data",
+        help="Name of the collection where documents are queried from (default: %(default)s)"
+    )
+
     args = parser.parse_args()
     return args
 
 
 def main(argv=None):
     args = parse_args(argv)
-    kwargs = {k: v for k, v in vars(args).items() if k != "cmd"}
+    kwargs = {k: v for k, v in vars(args).items() if "cmd" not in k}
     match args.cmd:
         case "run":
             asyncio.run(chat())
         case "collect":
             collect_data(**kwargs)
+        case "db":
+            match args.db_cmd:
+                case "create":
+                    create_vector_db(**kwargs)
+                case "query":
+                    print(query_vector_db(**kwargs)["documents"][0][0])
         case _:
             raise ValueError("Unknown command")
 
--- src/bird_id_assistant/db.py
+++ src/bird_id_assistant/db.py
@@ -1,11 +1,8 @@
-import argparse
 import os
 
 import chromadb
 from chromadb import QueryResult
 from tqdm import tqdm
-
-from bird_id_assistant.util import dir_path
 
 
 def create_vector_db(input_doc_dir: str, host="localhost", port=8000, collection_name="bia_data"):
@@ -39,45 +36,3 @@ def query_vector_db(
 
 def get_documents(results: list[QueryResult]):
     return results["documents"][0]
-
-
-def parse_args(argv=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default="localhost", help="ChromaDB client host URL")
-    parser.add_argument("--port", type=int, default=8000, help="ChromaDB client port")
-    subparsers = parser.add_subparsers(dest="cmd")
-
-    parser_create = subparsers.add_parser("create", help="Create vector database from text files in input dir")
-    parser_create.add_argument(
-        "input_doc_dir",
-        type=dir_path,
-        help="Directory containing text documents to store in vector database"
-    )
-
-    parser_query = subparsers.add_parser("query", help="Query documents from vector database")
-    parser_query.add_argument("query", type=str, help="Query string")
-    parser_query.add_argument(
-        "-n", "--n_results",
-        type=int,
-        default=1,
-        help="Number of closest matching documents to retrieve"
-    )
-
-    args = parser.parse_args()
-    return args
-
-
-def main(argv=None):
-    args = parse_args(argv)
-    kwargs = {k: v for k, v in vars(args).items() if k != "cmd"}
-    match args.cmd:
-        case "create":
-            create_vector_db(**kwargs)
-        case "query":
-            print([doc[:100] for doc in query_vector_db(**kwargs)["documents"][0]])
-        case _:
-            raise ValueError("Unknown command")
-
-
-if __name__ == "__main__":
-    main()
